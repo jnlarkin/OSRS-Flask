5e7490b4364c552092269ef05002490157b8e49b
jlark <j.larkin5945@gmail.com>
2024-09-11 14:16:52 -0500

    Fleshed out login, signup, and logout functions.

--- flask_app.py
+++ flask_app.py
@@ -8,14 +8,15 @@ app.secret_key = "123412hkasdjkas"
 
 db = SQLAlchemy(app)
 
+
 class users(db.Model):
     _id = db.Column("id", db.Integer, primary_key=True)
     name = db.Column(db.String(100))
-    email = db.Column(db.String(100))
+    password = db.Column(db.String(100))
 
-    def __init__(self, name, email):
+    def __init__(self, name, password):
         self.name = name
-        self.email = email
+        self.password = password
         pass
 
 @app.route("/")
@@ -31,44 +32,87 @@ def return_user(username):
             email = request.form["email"]
             session['email'] = email
             session_name = session['user']
-            found_email = users.query.filter_by(email=email).first()
-            if found_email:
-                pass
-            else:
-                pass
-                # add_usr = users(found_name, email)
-                # db.session.add(add_usr)
-                # db.session.commit()
+            found_name = users.query.filter_by(name=session_name).first()
+        return render_template('user.html', user=username, email=None)
 
 
-        elif 'email' in session['email']:
-            email = session['email']
-
-        return render_template('user.html', user=username, email=session['email'])
     else:
         return redirect(url_for('login'))
+
+@app.route('/signup', methods = ['POST', 'GET'])
+def signup():
+    if request.method == 'POST':
+        form_user = request.form['username']
+        form_password = request.form['password']
+
+        if form_user and form_password != '' or None:
+            add_usr = users(form_user, form_password)
+            db.session.add(add_usr)
+            db.session.commit()
+            session['user'] = form_user
+            return redirect(url_for('return_user', username=session['user']))
+        else:
+            print("please enter user and password")
+            return redirect(url_for('signup'))
+    else:
+        return render_template('signup.html')
 
 
 @app.route("/login", methods=['POST', 'GET'])
 def login():
+    error = None
     if request.method == 'POST':
-        form_user = request.form['nm']
-        form_email = request.form['email']
-        session['user'] = form_user
-        session['email'] = form_email
-        user = session['user']
-        email = session['email']
-        found_user = users.query.filter_by(name=user).first()
-        found_email = users.query.filter_by(email=email).first()
-        if found_user and found_email:
-            session['user'] = found_user
-            session['email'] = found_email
-        else:
-            add_usr = users(user, email)
-            db.session.add(add_usr)
-            db.session.commit()
 
-        return redirect(f'/user/{user}')
+        form_user = request.form['username']
+        form_password = request.form['password']
+
+        if form_user and form_password != '' or None:
+            found_user = users.query.filter_by(name=form_user).first()
+            if found_user:
+                if found_user.password == form_password:
+                    session['user'] = found_user.name
+                    return redirect(url_for('return_user', username=session['user']))
+                else:
+                    error = "Wrong username or password"
+                    flash('Wrong username or password', 'errors')
+                    print("wrong username or password")
+                    return render_template('login.html', error=error)
+            else:
+                error = "Wrong username or password"
+                flash('Wrong username or password', 'errors')
+                print("wrong username or password")
+                return render_template('login.html', error=error)
+        else:
+            print("please enter user and password")
+            return redirect(url_for('login'))
+
+
+
+        # session['user'] = form_user
+        # session['email'] = form_email
+        # user = session['user']
+        # email = session['email']
+        # found_user = users.query.filter_by(name=user).first()
+        # if found_user and found_user.email:
+        #     session['user'] = found_user
+        #     session['email'] = found_user.email
+        #     return redirect(f'/user/{session["user"]}')
+        # if found_user:
+        #     session['user'] = found_user
+        #     return redirect(f'/user/{session["user"]}')
+        # else:
+        #     if email:
+        #         add_usr = users(user, email)
+        #         db.session.add(add_usr)
+        #         db.session.commit()
+        #         return redirect(f'/user/{user}')
+        #     else:
+        #         add_usr = users(user, '')
+        #         db.session.add(add_usr)
+        #         db.session.commit()
+        #         return redirect(f'/user/{user}')
+
+
     elif 'user' in session:
         return redirect(url_for('return_user', username=session['user']))
     else:
@@ -78,8 +122,7 @@ def login():
 @app.route("/logout")
 def logout():
     session.pop('user', None)
-    session.pop('email', None)
-    flash('Logged out Successfully', 'info')
+    flash('Logged out Successfully', 'successes')
     return redirect(url_for('login'))
 
 
